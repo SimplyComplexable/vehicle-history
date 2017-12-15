eadb3bbdf47d71d4dfe7e8026e901579dbbd8e04
Alex <mustang.4222@yahoo.com>
2017-12-14 23:27:54 -0700

    updated home page

--- src/Views/Index.php
+++ src/Views/Index.php
@@ -10,11 +10,14 @@
     <a href='./' class='display-4 navbar-brand ml-2' style='font-size: 1.7em;'>Vehicle Service History Tracking System</a>
     <div class='float-right text-right'>
         <a class="text-light btn btn-primary mr-2 my-sm-0" href="./login">Login</a>
+        <a class="text-light btn btn-primary mr-2 my-sm-0" href="./register">Register</a>
     </div>
 </nav>
 <div class='container-fluid px-0'>
     <div class='mt-4 mx-auto alert alert-success px-4 py-4 ml-4' style='width: 72%'><h2 class='display-4 alert-heading pb-2'>Welcome!</h2>
-        <h3 class='lead' style="font-size: x-large">This website allows users to track all the service history/maintenance for their vehicle(s). The user will be able to login, add vehicles to their account, input maintenance records (i.e. oil changed, tires rotated, etc.), input fuel receipts, input parts, and view all the maintenance records, fuel expenses, and parts.</h3>
+        <h3 class='lead mb-4' style="font-size: x-large">This website allows you to track service history/maintenance records for your vehicle(s). You can add vehicles to your account, input maintenance records (i.e. oil changed, tires rotated, etc.), input fuel receipts, input parts, and view all the maintenance records, fuel expenses, and parts.</h3>
+        <h3 class='lead mb-4' style="font-size: x-large">To begin, simply create an account by clicking register. Then fill in the fields and you can begin adding vehicles and tracking info about them.</h3>
+        <h3 class='lead' style="font-size: x-large">If you already have an account, just login.</h3>
     </div>
     </div>
 </body>
